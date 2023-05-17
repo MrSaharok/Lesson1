c374b6e55ea605a49f2d171468a049cadf767e16
MrSaharok <111682001+MrSaharok@users.noreply.github.com>
2023-05-17 21:37:59 +0700

    Lesson1.rb

--- Lesson1.rb
+++ Lesson1.rb
@@ -1,1 +1,10 @@
- Lesson1
+print "Введите ваше имя:"
+a = gets.chomp.capitalize
+print "Введите ваш рост:"
+b = gets.chomp
+c = (b.to_i - 110)*1.15
+if c >=0
+puts "#{a} ваш вес должен быть #{c}"
+else 
+puts "#{a} ваш вес уже оптимальный"
+end
